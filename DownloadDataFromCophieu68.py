4de771f03f4830fe247cf26618f320413e224b25
Nghia Ho <hdn.hodacnghia.hdn@gmail.com>
2018-05-24 11:18:04 +0700

    data stock

--- DownloadDataFromCophieu68.py
+++ DownloadDataFromCophieu68.py
@@ -1,164 +1,189 @@
-import sys, time, os, csv, shutil, re, glob
+import sys
+import time
+import os
+import csv
+import shutil
+import re
+import glob
 from selenium import webdriver
+
 
 def get_driver(save_to_folder):
     save_path = os.path.join(current_director, save_to_folder)
-    
-    prefs = {'download.default_directory' : save_path}
+
+    prefs = {'download.default_directory': save_path}
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', prefs)
-    driver = webdriver.Chrome("D:/chromedriver_win32/chromedriver.exe", chrome_options=options)
-    
+    driver = webdriver.Chrome(
+        "C:/Users/love_/Downloads/Compressed/chromedriver.exe", chrome_options=options)
+
     return driver
 
-def login_to_web(driver, username = 'vttqhuy@gmail.com', password = 'Sinhvien1'):
+
+def login_to_web(driver, username='vttqhuy@gmail.com', password='Sinhvien1'):
     driver.get("http://www.cophieu68.vn")
-    
+
     loginElem = driver.find_element_by_link_text('Đăng nhập')
     loginElem.click()
     time.sleep(1)
-    
+
     usernameElem = driver.find_element_by_name("username")
     passwordElem = driver.find_element_by_name("tpassword")
     usernameElem.send_keys(username)
     passwordElem.send_keys(password)
     passwordElem.submit()
 
+
 def download_stockIDs_from_netfonds(driver, market_name, save_filename):
-    driver.get('http://www.netfonds.no/quotes/exchange.php?exchange=' + market_name)
+    driver.get(
+        'http://www.netfonds.no/quotes/exchange.php?exchange=' + market_name)
     time.sleep(1)
-    
+
     list_stockID = []
-    
+
     links = driver.find_elements_by_class_name('left')
     links = links[1:]
-    
+
     for l in links:
         tag_a = l.find_element_by_css_selector('a')
         list_stockID.append(tag_a.text)
-    
+
     save_list_stockID_to_file(list_stockID, save_filename)
+
 
 def download_vnindex_stockIDs(driver):
     driver.get('http://www.cophieu68.vn/calculating_market_index.php?id=^vnindex')
     time.sleep(1)
-    
+
     login_to_web(driver)
     time.sleep(1)
-    
+
     list_stockID = []
     pageCurrent = 1
     total_pages = 15
-    
+
     while True:
         # TODO: Go to stock list
         tableElem = driver.find_element_by_id('fred')
         rowElem = tableElem.find_elements_by_css_selector('tr')
-        
+
         # TODO: Delete Header
         rowElem = rowElem[1:]
-        
+
         for r in rowElem:
             columnElems = r.find_elements_by_css_selector('td')
-            
-            #TODO get link stock in second column
+
+            # TODO get link stock in second column
             link = columnElems[1].find_element_by_css_selector('a')
             list_stockID.append(link.text)
-            
+
         pageCurrent += 1
-        
+
         if (pageCurrent > total_pages):
             break
-        driver.get("http://www.cophieu68.vn/calculating_market_index.php?currentPage=" + str(pageCurrent) + "&id=^vnindex")
+        driver.get("http://www.cophieu68.vn/calculating_market_index.php?currentPage=" +
+                   str(pageCurrent) + "&id=^vnindex")
         time.sleep(1)
-    
+
     save_list_stockID_to_file(list_stockID, "stockID_vnindex.txt")
+
 
 def download_hnxindex_stockIDs(driver):
     driver.get('http://www.cophieu68.vn/calculating_market_index.php?id=^hastc')
     time.sleep(1)
-    
+
     login_to_web(driver)
     time.sleep(1)
-    
+
     list_stockID = []
     pageCurrent = 1
     total_pages = 16
-    
+
     while True:
         # TODO: Go to stock list
         tableElem = driver.find_element_by_id('fred')
         rowElem = tableElem.find_elements_by_css_selector('tr')
-        
+
         # TODO: Delete Header
         rowElem = rowElem[1:]
-        
+
         for r in rowElem:
             columnElems = r.find_elements_by_css_selector('td')
-            
-            #TODO get link stock in second column
+
+            # TODO get link stock in second column
             link = columnElems[1].find_element_by_css_selector('a')
             list_stockID.append(link.text)
-            
+
         pageCurrent += 1
-        
+
         if (pageCurrent > total_pages):
             break
-        driver.get("http://www.cophieu68.vn/calculating_market_index.php?currentPage=" + str(pageCurrent) + "&id=^hastc")
+        driver.get("http://www.cophieu68.vn/calculating_market_index.php?currentPage=" +
+                   str(pageCurrent) + "&id=^hastc")
         time.sleep(1)
-    
+
     save_list_stockID_to_file(list_stockID, "stockID_hnxindex.txt")
+
 
 def save_list_stockID_to_file(listStockID, fileName):
     theFile = open(fileName, 'w')
     theFile.write('\n'.join(listStockID))
     theFile.close()
-    
+
+
 def read_list_stockID_from_file(filePath):
     theFile = open(filePath, 'r')
     content = theFile.read()
     listStockID = content.split('\n')
     return listStockID
 
+
 def download_history_price(stockID):
-    driver.get("http://www.cophieu68.vn/export/excel.php?id=" + stockID + "&df=&dt=")
-    
+    driver.get("http://www.cophieu68.vn/export/excel.php?id=" +
+               stockID + "&df=&dt=")
+
+
 def crawl_data_from_netfond(driver, folder_to_save, ticker, exchange):
-    driver.get('https://www.netfonds.no/quotes/paperhistory.php?paper=' + ticker + '.' + exchange + '&csv_format=csv')
-    
-    data = [['<Ticker>', '<DTYYYYMMDD>', '<Open>', '<High>', '<Low>', '<Close>', '<Volume>']]
+    driver.get('https://www.netfonds.no/quotes/paperhistory.php?paper=' +
+               ticker + '.' + exchange + '&csv_format=csv')
+
+    data = [['<Ticker>', '<DTYYYYMMDD>', '<Open>',
+             '<High>', '<Low>', '<Close>', '<Volume>']]
     content = driver.find_element_by_css_selector('pre')
-    
+
     rows = content.text.split('\n')
     rows = rows[1:]
-    
+
     for r in rows:
         r_data = r.split(',')
-        data.append([r_data[1], r_data[0], r_data[3], r_data[4], r_data[5], r_data[6], r_data[7]])
-    
+        data.append([r_data[1], r_data[0], r_data[3],
+                     r_data[4], r_data[5], r_data[6], r_data[7]])
+
     folder_to_save = os.path.join(os.getcwd(), folder_to_save)
     file = open(os.path.join(folder_to_save, ticker + '.csv'), 'w', newline='')
     with file:
         writer = csv.writer(file)
         writer.writerows(data)
 
+
 def crawl_stockID_in_tradingeconomic(driver, market_name, save_to):
     stockIDs = []
     driver.get('https://tradingeconomics.com/' + market_name + '/stock-market')
-    
-    showmore = driver.find_element_by_css_selector('svg')
-    showmore.click()
-    
+
+    # showmore = driver.find_element_by_css_selector('svg')
+    # showmore.click()
+
     div_contains_table = driver.find_element_by_class_name('table-minimize')
-    
+
     rows = div_contains_table.find_elements_by_css_selector('tr')
     for r in rows[1:]:
         txt = r.text
         stockID = txt.split(' ')[0]
         stockIDs.append(stockID)
     save_list_stockID_to_file(stockIDs, save_to)
-    
-current_director = os.getcwd()   
+
+
+current_director = os.getcwd()
 os.makedirs('dulieuvnindex', exist_ok=True)
 os.makedirs('dulieuhnxindex', exist_ok=True)
 #savePath = os.path.join(current_director, "dulieuvnindex")
@@ -291,14 +316,19 @@ for i in nasdaq_stockIDs:
     time.sleep(2)
 '''
 
-#shutil.rmtree('dulieunikkei225', ignore_errors=True)
-driver = get_driver('dulieunikkei225')
-#crawl_stockID_in_tradingeconomic(driver, 'japan', 'stockID_nikkei225.txt')
-nekkei_stockIDs = read_list_stockID_from_file('stockID_nikkei225.txt')
+shutil.rmtree('dulieuFTSEMIB', ignore_errors=True)
+driver = get_driver('dulieuFTSEMIB')
+crawl_stockID_in_tradingeconomic(
+    driver, 'italy', 'stockID_FTSEMIB.txt')
+nekkei_stockIDs = read_list_stockID_from_file('stockID_FTSEMIB.txt')
 
 for i in nekkei_stockIDs:
-    driver.get('https://finance.yahoo.com/quote/' + i + '.T/history?period1=1199120400&period2=1526749200&interval=1d&filter=history&frequency=1d')
+    driver.get('https://finance.yahoo.com/quote/' + i +
+               '.MI/history?period1=1199120400&period2=1526749200&interval=1d&filter=history&frequency=1d')
     time.sleep(2)
-    a = driver.find_element_by_link_text('Download Data')
-    a.click()
+    try:
+        a = driver.find_element_by_link_text('Download Data')
+        a.click()
+    except ValueError:
+        continue
     time.sleep(2)
